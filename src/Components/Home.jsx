22def5d75fb71a7916362c5b8364121ce52e1e65
Jhon Hernández <96154373+jhobahego@users.noreply.github.com>
2023-12-21 23:10:59 +0000

    Update title and subtitle position

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -29,10 +29,12 @@ const Home = ({ name, title }) => {
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
       <div
-        style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem", color: "white" }}
+        style={{ position: "absolute", top: "7rem", left: "4rem", width: "17rem", color: "white" }}
       >
         <h1 className="title">{name}</h1>
-        <h2 className="subtitle" style={{ opacity: 0.7 }}>{title}</h2>
+        <h2 className="subtitle" style={{ opacity: 0.7 }}>
+          {title}
+        </h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img
